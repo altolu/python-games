3be0be65ce829b5264fb37c7444e79739f6fff30
altolu.llc@gmail.com <altolu.llc@gmail.com>
2020-04-27 19:40:41 -1000

    Generated test grid of values and filled board with red fire or blue ice based on values grid of moves- evenfor ice, odd for fire

--- ConnectFour.py
+++ ConnectFour.py
@@ -7,7 +7,17 @@ class ConnectFour:
         self.player_color = color
         self.red_fire = '\U0001F525' #🔥
         self.blue_ice = '\U0001F9CA' #🧊
+        self.rows = 6
+        self.columns = 7
+        self.grid = []
         #pieces_on_board = [[], []]
+
+    def init_pieces_array(self):
+        counter = 0
+        for _ in range(self.columns):
+            new_col = [counter+num for num in range(1, self.rows+1)]
+            counter += self.rows
+            self.grid.append(new_col)
 
     def welcome_player(self):
         c = 'blue'
@@ -16,24 +26,46 @@ class ConnectFour:
         print(f'Red is fire {self.red_fire} and Blue is ice {self.blue_ice}')
         print('\n')
 
-    def print_board(self, rows=6, columns=7):
-        board = ""
-        # Divider Rows
-        top_row = '⟔-------------------------------------'
-        separator_row = '||----------------------------------||'
-        bottom_row = '-------------------------------------⟓'
+
+    def get_content_row(self, row):
+        new_row = ''
         # Token Rows
         empty_block = '    '
         fire_block = ' 🔥 '
         ice_block = ' 🧊 '
         outer_wall = '||'
         inner_wall = '|'
+
+        row_data = []
+        for col in self.grid:
+            row_data.append(col[row])
+
+        new_row += outer_wall
+        for d in row_data:
+            if d%2 == 0:
+                new_row += ice_block
+            else:
+                new_row += fire_block
+            new_row += inner_wall
+        new_row = new_row[:-1] # Removing last extraneous inner_wall
+        new_row += outer_wall
+        return new_row
+
+    def print_board(self):
+        board = ""
+        # Divider Rows
+        top_row = '⟔-------------------------------------'
+        separator_row = '||----------------------------------||'
+        bottom_row = '-------------------------------------⟓'
+
         temp_content_row = '||    |    |    |    |    |    |    ||'
 
         board += top_row + '\n'
-        for row in range(rows):
-            board += temp_content_row + '\n'
-            if row < rows-1:
+        for row in range(self.rows):
+            # board += temp_content_row + '\n'
+            reverse_counter = self.rows - (row+1)
+            board += self.get_content_row(reverse_counter) + '\n'
+            if row < self.rows-1:
                 board += separator_row + '\n'
         board += bottom_row + '\n'
 
@@ -85,5 +117,8 @@ class ConnectFour:
 
 if __name__ == '__main__':
     game = ConnectFour()
+    game.init_pieces_array()
+    print(f'Grid: {game.grid}')
     game.welcome_player()
     game.print_board()
+    game.init_pieces_array()
